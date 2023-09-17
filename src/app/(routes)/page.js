12ec6dea577227556121947a676c23d5b0cda4e9
ahmed <elshehry@gamil.com>
2023-09-17 17:28:24 +0300

    small fix on main billboard

--- src/app/(routes)/page.js
+++ src/app/(routes)/page.js
@@ -1,19 +1,21 @@
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/Container";
-import getBillboard from "../actions/get-billboard";
+
 import getProducts from "../actions/get-products";
 import ProductList from "@/components/product-list";
+import getBillboards from "../actions/get-billboards"
 
 export const revalidate = 0;
 
 export default async function Home() {
+  const billboards=await getBillboards();
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard('2cc53611-4ece-4312-9323-a34194c982a7');
+  const billboard=billboards.filter(billboard=>billboard.label==='main');
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={billboard} />
+        <Billboard data={billboard?billboard[0]: billboards[0]} />
         <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
           <ProductList title={'Featured Products'} items={products} />
         </div>
---
+++ src/app/actions/get-billboards.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const url = `${process.env.NEXT_PUBLIC_API_URL}/billboards`
+
+const getBillboards = async () => {
+    try {
+        let data = await axios.get(url);
+        return data.data;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export default getBillboards;
