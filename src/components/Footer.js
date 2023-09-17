86e0fab46913f2fd939dd40ee5b7901afc776881
ahmed <elshehry@gamil.com>
2023-09-17 18:27:18 +0300

    Fixed the footer date

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 const Footer = () => {
+    const currentYear=new Date();
     return (
         <footer className="bg-white border-t">
             <div className="mx-auto py-10">
                 <p className="text-center text-xs text-black">
-                    &copy; 2023 store Name, Inc. All rights reserved.
+                    &copy; {currentYear.getFullYear()} E-store, Inc. All rights reserved.
                 </p>
             </div>
         </footer>
